a0bb8eaf30d208a2ea3fa649ddc9d7c037b2be15
Bibikto Roy Das <bibikto@iMac.local>
2020-11-29 23:25:57 +0530

    ..

--- src/components/AppBarComp.js
+++ src/components/AppBarComp.js
@@ -10,19 +10,6 @@ const useStyles = (theme) => ({
         color: 'white'
     }
 })
-function mapStateToProps(state) {
-    return {
-        currentUser: state.currentUser
-    }
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-        setUser: (userObj) => {
-            dispatch({ type: "SET_USER", payload: userObj })
-        }
-    }
-}
 
 class AppBarCompClass extends React.Component {
     componentDidMount = () => {
@@ -36,22 +23,17 @@ class AppBarCompClass extends React.Component {
         return (
             <AppBar className="appBarColor" position="static">
                 <Toolbar className='toolBarRight'>
-                    {this.props.currentUser.name == "" ? (
+                    
                         <Link to='/login' className={classes.Link}>
                             <Button color="inherit">Login</Button>
                         </Link>
-                    ) : (
-                            <Avatar>{this.props.currentUser.name.charAt(0)}</Avatar>
-                        )
-                    }
-
+                   
                 </Toolbar>
             </AppBar>
         )
     }
 }
 
-const AppBarCompT = withStyles(useStyles)(AppBarCompClass)
-const AppBarComp = connect(mapStateToProps, mapDispatchToProps)(AppBarCompT)
+const AppBarComp = withStyles(useStyles)(AppBarCompClass)
 
 export default AppBarComp

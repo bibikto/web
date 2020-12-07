cd4437ecd750d1913eb2dd5ef17b6fed3698ae35
Bibikto Roy Das <bibikto@iMac.local>
2020-12-07 22:33:47 +0530

    Add Build

---
+++ build/precache-manifest.5ac65fd582768ef3f09f2eb576970f3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1fbe7a614d2ea88f13e5afb6d62bd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce2ae0958b056023263a",
+    "url": "/static/css/main.c5659579.chunk.css"
+  },
+  {
+    "revision": "19393ef13ba65f974940",
+    "url": "/static/js/2.050108a4.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.050108a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2ae0958b056023263a",
+    "url": "/static/js/main.7a1095b3.chunk.js"
+  },
+  {
+    "revision": "ead25b3f871ab0029a85",
+    "url": "/static/js/runtime-main.68c74421.js"
+  }
+]);
